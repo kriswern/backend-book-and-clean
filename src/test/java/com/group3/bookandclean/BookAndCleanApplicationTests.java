a498560c603d4730c372a21fd26f38cc7bd614d8
Coin-byte <kriswern@icloud.com>
2021-12-02 11:24:36 +0100

    initial commit

---
+++ src/test/java/com/group3/bookandclean/BookAndCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.bookandclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAndCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
