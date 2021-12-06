88208a4246a6359f1298c910c3f7c012adec9c90
saurabh311 <saurabh311@gmail.com>
2021-12-06 11:28:39 +0100

    added UserDetailSevices package

--- src/main/java/com/group3/bookandclean/login/CustomUserDetailsService.java
+++ src/main/java/com/group3/bookandclean/login/CustomUserDetailsService.java
@@ -1,2 +1,23 @@
-package com.group3.bookandclean.login;public class CustomUserDetailsService {
+package com.group3.bookandclean.login;
+
+import com.group3.bookandclean.entity.User;
+import com.group3.bookandclean.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        // Email is used as the username in this case
+        User user = userRepository.findByEmail(username);
+        if (user == null) throw new UsernameNotFoundException(username);
+        return new org.springframework.security.core.userdetails.User("user.getEmail()", user.getPassword(), new ArrayList<>());
+    }
 }
