6a08ef530eebd2e717d43cb80ae47518b966af9d
saurabh311 <saurabh311@gmail.com>
2021-12-06 23:41:43 +0100

    added register controller

--- src/main/java/com/group3/bookandclean/controller/RegisterController.java
+++ src/main/java/com/group3/bookandclean/controller/RegisterController.java
@@ -4,19 +4,17 @@ import com.group3.bookandclean.entity.Customer;
 import com.group3.bookandclean.entity.User;
 import com.group3.bookandclean.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoginController {
+public class RegisterController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to Cleaning Booking Portal!!";
+    @PostMapping("/register")
+    public void register(@RequestBody User user) {
+        userService.register(user);
     }
 }
