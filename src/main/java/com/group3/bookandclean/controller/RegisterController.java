3bdcf9a7985314e1ea2b6a03607f01d35ce7aec5
saurabh311 <saurabh311@gmail.com>
2021-12-07 17:27:45 +0100

    added register controller

--- src/main/java/com/group3/bookandclean/controller/RegisterController.java
+++ src/main/java/com/group3/bookandclean/controller/RegisterController.java
@@ -1,20 +1,67 @@
 package com.group3.bookandclean.controller;
 
+import com.group3.bookandclean.entity.Cleaner;
 import com.group3.bookandclean.entity.Customer;
 import com.group3.bookandclean.entity.User;
-import com.group3.bookandclean.services.UserService;
+import com.group3.bookandclean.repository.CleanerRepository;
+import com.group3.bookandclean.repository.CustomerRepository;
+import com.group3.bookandclean.repository.UserRepository;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class RegisterController {
     @Autowired
-    UserService userService;
+    UserRepository userRepository;
 
-    @PostMapping("/register")
-    public void register(@RequestBody User user) {
-        userService.register(user);
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CleanerRepository cleanerRepository;
+
+    @PostMapping(value = "/register")
+    public boolean register(@RequestBody RegisterForm registerForm) {
+        User user = User.builder()
+                .email(registerForm.getEmail())
+                .password(registerForm.getPassword())
+                .type(registerForm.getType())
+                .build();
+        userRepository.save(user);
+
+        if (registerForm.type.equals("Cleaner")){
+            Cleaner cleaner = Cleaner.builder()
+                    .name(registerForm.getName())
+                    .address(registerForm.getAddress())
+                    .user(user)
+                    .build();
+            cleanerRepository.save(cleaner);
+
+        }else {
+            Customer customer = Customer.builder()
+                    .name(registerForm.getName())
+                    .address(registerForm.getAddress())
+                    .user(user)
+                    .build();
+            customerRepository.save(customer);
+        }
+        return true;
     }
 }
+
+@Data
+class RegisterForm {
+    String name;
+    String email;
+    String password;
+    String address;
+    String type;
+}
+
--- src/main/java/com/group3/bookandclean/services/CustomUserDetailsService.java
+++ src/main/java/com/group3/bookandclean/services/CustomUserDetailsService.java
@@ -18,6 +18,6 @@ public class CustomUserDetailsService implements org.springframework.security.co
         // Email is used as the username in this case
         User user = userRepository.findByEmail(username);
         if (user == null) throw new UsernameNotFoundException(username);
-        return new org.springframework.security.core.userdetails.User("user.getEmail()", user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 }
