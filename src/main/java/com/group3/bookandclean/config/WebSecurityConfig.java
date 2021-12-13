45b9b6badd9e2ab255d4bde4f30e5440ed2ec703
Coin-byte <kriswern@icloud.com>
2021-12-13 12:54:07 +0100

    Authority for endpoints and password encryption

--- src/main/java/com/group3/bookandclean/config/WebSecurityConfig.java
+++ src/main/java/com/group3/bookandclean/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -37,7 +38,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login", "/api/admin/assigncleaner", "/api/admin/bookings", "/api/admin/cleaners", "/api/cleaner/bookings", "/api/admin/customers", "/api/admin/addbooking").permitAll()
+                .antMatchers("/login", "/register").permitAll()
+                .antMatchers("/api/admin/**").hasAuthority("admin")
+                .antMatchers("/api/cleaner/**").hasAuthority("cleaner")
+                .antMatchers("/api/customer/**").hasAuthority("customer")
                 .anyRequest().authenticated().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
@@ -50,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(); //You could add an integer to make the crypt stronger
     }
 }
 
